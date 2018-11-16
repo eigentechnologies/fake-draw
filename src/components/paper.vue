47036c5a5b75034314fcb5f01cb7a37ab1faf850
Jacob Cozens <jacob.cozens@eigentech.com>
2018-11-16 16:14:49 +0000

    clear canvas

--- src/components/paper.vue
+++ src/components/paper.vue
@@ -7,26 +7,41 @@ import CanvasFreeDrawing from 'canvas-free-drawing'
 
 export default {
   name: 'Paper',
+  data: function () {
+    return {
+      paper: null
+    }
+  },
   methods: {
     isDrawing: function() {
       this.$emit('drawing');
     }
   },
+  props: ['drawObject'],
+  watch: {
+  	drawObject: function(newVal, oldVal) { // watch it
+      console.log('Prop changed: ', newVal, ' | was: ', oldVal)
+      if (newVal !== oldVal) {
+        this.paper.clear()
+      }
+    }
+  },
   mounted() {
+    let that = this;
     this.$nextTick(function() {
       // initialize
-      const paper = new CanvasFreeDrawing({
+      that.paper = new CanvasFreeDrawing({
         elementId: 'paper',
         width: 750,
         height: 500
       });
 
       // set properties
-      paper.setLineWidth(10); // in px
-      paper.setStrokeColor([0, 0, 255]); // in RGB
+      that.paper.setLineWidth(10); // in px
+      that.paper.setStrokeColor([0, 0, 255]); // in RGB
 
       // listen to events
-      paper.on({
+      that.paper.on({
         event: 'redraw'
       }, () => {
         this.isDrawing();
